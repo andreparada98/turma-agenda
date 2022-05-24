c8a80d8bb03cede157cb0faa38a0c61f7cf2c3d2
Mariana Lino <marianas2lino@gmail.com>
2022-05-24 19:56:37 -0300

    Event Binding

---
+++ Aulas/08_conceitos_angular/app-conceitos/src/app/components/event-binding/event-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-event-binding',
+	templateUrl: './event-binding.component.html',
+	styleUrls: ['./event-binding.component.scss']
+})
+
+export class EventBindingComponent {
+	escola: string = "Impacta";
+
+	alterarTexto(): void {
+		this.escola = "Impacta Tecnologia";
+	}
+}
